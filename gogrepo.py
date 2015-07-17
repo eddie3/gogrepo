105aec08d42d44b558ce7970e53d0b022483e083
eddie3 <desynch@gmail.com>
2015-07-17 01:32:01 -0400

    Fix 'update' command which missed download links. GOG changed their JSON API and made the downloads info dict a list.

--- gogrepo.py
+++ gogrepo.py
@@ -249,11 +249,12 @@ def fetch_file_info(d, fetch_md5):
                     else:
                         raise
 
-def filter_downloads(out_list, downloads_dict, lang_list, os_list):
+def filter_downloads(out_list, downloads_list, lang_list, os_list):
     """filters any downloads information against matching lang and os, translates
     them, and extends them into out_list
     """
     filtered_downloads = []
+    downloads_dict = dict(downloads_list)
 
     # hold list of valid languages languages as known by gogapi json stuff
     valid_langs = []
